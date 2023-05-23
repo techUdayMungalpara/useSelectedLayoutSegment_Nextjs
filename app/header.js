f6fae9d1716cbdf0d754cb690b461af2aa5ec0a0
Uday Mungalpara <techudaymungalpara@gmail.com>
2023-05-23 14:35:17 +0530

    Commit message 1st

--- app/header.js
+++ app/header.js
@@ -4,6 +4,12 @@ const Header = () => {
     <div>
       <Navlink href="/movie">Dashboard</Navlink>
       <Navlink href="/ram">next</Navlink>
+      <ul className=" flex flex-row items-center justify-between ">
+        <li className="p-2 bg-red-300 ">1</li>
+        <li className=" bg-cyan-200 p-2 ">2</li>
+        <li className=" p-2 bg-violet-500 ">3</li>
+        <li className=" p-2 bg-cyan-300 ">4</li>
+      </ul>
     </div>
   );
 };
